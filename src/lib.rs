57e8edef905cc99e3c1ae80fab89adab451072da
0xdipak <dipaksharma34447255@gmail.com>
2025-12-01 16:27:30 +0545

    optimized for better logging...

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use std::net::TcpListener;
 pub mod configuration;
 pub mod routes;
 pub mod startup;
+pub mod telementry;
 
 #[derive(serde::Deserialize)]
 struct Formdata {
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,8 +1,24 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
+use std::io::sink;
 use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{DatabaseSettings, get_configuration};
 use zero2prod::startup::run;
+use zero2prod::telementry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 
 pub struct TestApp {
     pub address: String,
@@ -10,11 +26,18 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
+
+    let subscriber = get_subscriber("test".into(), "debug".into(), sink);
+    init_subscriber(subscriber);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bindrandom port");
+
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
     let mut configuration = get_configuration().expect("Failed to read configurations");
+
     configuration.database.database_name = Uuid::new_v4().to_string();
 
     let connection_pool = configure_database(&configuration.database).await;
